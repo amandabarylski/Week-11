9b4a51089fd8c511bc2ce762922d1dc4b90758f3
Amanda Barylski <amandabarylski@live.com>
2024-08-15 21:24:34 -0400

    trying some scripting

--- script.js
+++ script.js
@@ -18,3 +18,15 @@
 $('button').on('click', () => {
     $('.game-space').text('')
 })
+
+//I could not work out how to select only the clicked element in a class.
+//I added ids to all of my game spaces, which seems incredibly inefficient but I couldn't see another way.
+//Since there would have to be 9 click events now, I instead have them reference outside functions that I only type once.
+const drawSymbol = (clickedSpace) => clickedSpace.text($('b').text())
+//I made one to do a quick test, and it worked! Still seemed far too inefficient, though, so I went to
+//our instructor's repository and found he'd done something different.
+//When I attempted it, it didn't work, maybe because my game board is an id, maybe because my spaces are in rows.
+//I'll be working on a fix later.
+$('#top-left').on('click', () => {
+    drawSymbol($('#top-left'))
+})
